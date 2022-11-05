297a0d93e5aa65fa231eb56365fc28abfa5cafe7
zaina-allan <75915075+zaina-allan@users.noreply.github.com>
2022-11-05 17:26:49 +0200

    Add files via upload

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include,path
+from users.views import dashboard,register
+urlpatterns = [
+path("dashboard/", dashboard, name="dashboard"),
+path("accounts/", include("django.contrib.auth.urls")),
+# path('oauth/', include('social_django.urls', namespace='social')), # <-- here
+path("register/", register, name="register"),
+]
